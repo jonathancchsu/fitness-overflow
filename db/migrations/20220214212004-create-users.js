bcbdd701fcd091820d3376a79d3db3b039350ca1
Nick Austin <nickaustin@Nicks-Mac-mini.local>
2022-02-15 17:14:32 -0700

    FINALLY

--- db/migrations/20220214212004-create-users.js
+++ db/migrations/20220214212004-create-users.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.STRING,
         unique: true
       },
-      password: {
+      hashedPassword: {
         allowNull: false,
         type: Sequelize.STRING
       },
