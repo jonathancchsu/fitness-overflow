fd98c18abf1e9befd40536fa4486b9484224585e
Jonathan H <jhsucoding@gmail.com>
2022-02-15 19:42:18 -0500

    added questions route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
+const { asyncHandler, csrfProtection } = require('./utils');
+const db = require('../db/models');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/:id', asyncHandler(async function(req, res, next) {
+  const userId = req.params.id;
+  const user = await db.User.findByPk(userId);
+
+  if (user) {
+
+  } else {
+
+  }
+  
   res.send('respond with a resource');
-});
+}));
 
 module.exports = router;
