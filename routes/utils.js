7c0065b10ef92458161b8b726aed3d9170f7e320
Jonathan H <jhsucoding@gmail.com>
2022-02-16 15:59:10 -0500

    questions routes completed

--- routes/utils.js
+++ routes/utils.js
@@ -6,4 +6,19 @@ const asyncHandler = (handler) => (req, res, next) =>
     handler(req, res, next)
         .catch(next);
 
-module.exports = { csrfProtection, asyncHandler };
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map((error) => error.msg);
+
+        const err = Error("Bad request.");
+        err.status = 400;
+        err.title = "Bad request.";
+        err.errors = errors;
+        return next(err);
+    }
+    next();
+};
+
+module.exports = { csrfProtection, asyncHandler, handleValidationErrors };
